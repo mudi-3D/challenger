6d530be17eeeb67c39298736aa57cb95f5487d2c
mudi-3D <soporte@mudi.com.co>
2025-02-05 17:41:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ createStyles(){
     link = document.createElement('LINK');
     link.setAttribute('rel','stylesheet');
     link.id="stylesMudiGeneral";
-    link.href=`https://cdn.jsdelivr.net/gh/RodriguezJose92/challenger@latest/index.css`; /* custom this path */
+    link.href=`https://cdn.jsdelivr.net/gh/mudi-3D/challenger@latest/index.css`; /* custom this path */
     
     document.head.appendChild(link);
 };
@@ -165,28 +165,28 @@ createModal(){
 
                         <div class="titleContainer">
                             <div class="iconTitle">
-                                <img class="stepMudi step1" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/challenger@latest/assets/step3.webp">
+                                <img class="stepMudi step1" src="https://cdn.jsdelivr.net/gh/mudi-3D/challenger@latest/assets/step3.webp">
                             </div>
                             <p class="textInfoMudi">Apunta el teléfono al piso.</p>
                         </div>
 
                         <div class="titleContainer">
                             <div class="iconTitle">
-                                <img class="stepMudi step2" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/challenger@latest/assets/step4.webp">
+                                <img class="stepMudi step2" src="https://cdn.jsdelivr.net/gh/mudi-3D/challenger@latest/assets/step4.webp">
                             </div>
                             <p class="textInfoMudi">Desplaza para visualizar.</p>
                         </div>
 
                         <div class="titleContainer">
                             <div class="iconTitle">
-                                <img class="stepMudi step3" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/challenger@latest/assets/step2.webp">
+                                <img class="stepMudi step3" src="https://cdn.jsdelivr.net/gh/mudi-3D/challenger@latest/assets/step2.webp">
                             </div>
                             <p class="textInfoMudi">Amplia y detalla el producto.</p>
                         </div>
 
                         <div class="titleContainer">
                             <div class="iconTitle">
-                                <img class="stepMudi step4" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/challenger@latest/assets/step1.webp">
+                                <img class="stepMudi step4" src="https://cdn.jsdelivr.net/gh/mudi-3D/challenger@latest/assets/step1.webp">
                             </div>
                             <p class="textInfoMudi">Toca dos veces para restablecer.</p>
                         </div>
